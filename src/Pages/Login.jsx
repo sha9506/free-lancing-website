5263bc88e19095fd79306ae90926256247f858a0
sabnam <sabnamhazari@gmail.com>
2024-06-13 13:15:12 +0530

    api fetch

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import Banner from '../Assets/banner.png';
 import { useNavigate } from "react-router-dom";
 
 const Login=()=>
 {
-
     const navigate = useNavigate();
+    const[loginMsg, setLoginMsg]=useState('');
+    const[loginEmail, setLoginEmail]=useState('');
+    const[loginPassword, setLoginPassword]=useState('');
+
+    const handleLogin=()=>
+    {
+        const myHeaders = new Headers();
+            myHeaders.append("Content-Type", "application/json");
+
+        const raw = JSON.stringify({
+            "email": loginEmail,
+            "password": loginPassword
+        });
+
+        const requestOptions = {
+            method: "POST",
+            headers: myHeaders,
+            body: raw,
+            redirect: "follow"
+        };
+
+        fetch("http://127.0.0.1:8000/login/", requestOptions)
+            .then((response) => response.text())
+            .then((result) => setLoginMsg(result))
+            .catch((error) => setLoginMsg(error));
+    }
+    
 
     return(<div className="login">
         <img className="banner-img" src={Banner} alt="banner" />
@@ -16,15 +42,16 @@ const Login=()=>
                 <div className="login-intro">Enter your email and password to access your account</div>
                 <div className="login-form">
                     <div className="lf-type">Email</div>
-                    <input className="lf-input" type="text" placeholder="Enter your email" />
+                    <input className="lf-input" type="text" placeholder="Enter your email" onChange={(e)=>{setLoginEmail(e.target.value)}}/>
                     <div className="lf-type">Password</div>
-                    <input className="lf-input" type="password" placeholder="Enter your password" />
+                    <input className="lf-input" type="password" placeholder="Enter your password" onChange={(e)=>{setLoginPassword(e.target.value)}}  />
                     <div className="remember-me">
                         <div><input type="checkbox" /> Remember Me</div>
                         <button className="forgot-pass">Forgot Password</button>
                      </div>
-                     <button className="login-button" onClick={()=>{navigate('/home')}}>Sign In</button>
+                     <button className="login-button" onClick={handleLogin}>Log In</button>
                     <button className="sign-up-button" onClick={()=>{navigate('/signup')}}>Register New Account</button>
+                    <div className="red">{loginMsg}</div>
                 </div>
                 
             </div>
--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -1,33 +1,87 @@
 import React from "react";
 import Banner from '../Assets/banner.png';
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const Signup=()=>
 {
+    const[firstName, setFirstName]=useState('');
+    const[lastName, setLastName]=useState('');
+    const[dob, setDob]=useState('');
+    const[email, setEmail]=useState('');
+    const[phone, setPhone]=useState('');
+    const[password, setPassword]=useState('');
+    const[rePassword, setRePassword]= useState('');
+    const[userType, setUserType]=useState('employee');
 
     const navigate = useNavigate();
+    const[message, setMessage]=useState('');
+    const[passwordMsg, setPasswordMsg]=useState('');
+
+    const handleSignin=()=>
+    {
+        if(password!==rePassword)
+        {
+            setPasswordMsg("Password does not match")
+        }
+
+        const myHeaders = new Headers();
+            myHeaders.append("Content-Type", "application/json");
+
+        const raw = JSON.stringify({
+            "first_name": firstName,
+            "last_name": lastName,
+            "dob": dob,
+            "email":email,
+            "phone": phone,
+            "password": password,
+            "user_type": userType
+        });
+
+        const requestOptions = {
+            method: "POST",
+            headers: myHeaders,
+            body: raw,
+            redirect: "follow"
+        };
+
+        fetch("http://127.0.0.1:8000/sign-up/", requestOptions)
+            .then((response) => {
+                response.text().then((data)=>setMessage(data))
+                if(response.status === 200)
+                {
+                    navigate('/login')
+                }
+            })
+            .catch((error) => setMessage(error));
+    
+    }
+
 
     return(<div className="login">
         <img className="banner-img" src={Banner} alt="banner" />
         <div className="login-input">
-            <div className="login-title"><span className="grey-text">Show</span>casy.</div>
+            <div className="login-title"><span className="red">{message}</span><span className="grey-text">Show</span>casy.</div>
             <div className="signup-input-box">
                 <div className="login-welcome">Welcome You</div>
                 <div className="login-intro">Enter your details to get started and access your account</div>
                 <div className="signup-form">
-                    <div className="sf-type">Name</div>
-                    <input className="sf-input" type="text" placeholder="Enter your name" />
+                    <div className="sf-type">First Name</div>
+                    <input className="sf-input" type="text" placeholder="Enter your name" onChange={(e)=>{setFirstName(e.target.value)}}/>
+                    <div className="sf-type">Last Name</div>
+                    <input className="sf-input" type="text" placeholder="Enter your name" onChange={(e)=>{setLastName(e.target.value)}} />
                     <div className="sf-type">Phone Number</div>
-                    <input className="sf-input" type="number" placeholder="Enter your phone number" />
+                    <input className="sf-input" type="number" placeholder="Enter your phone number" onChange={(e)=>{setPhone(e.target.value)}} />
                     <div className="sf-type">Email</div>
-                    <input className="sf-input" type="email" placeholder="Enter your email" />
+                    <input className="sf-input" type="email" placeholder="Enter your email" onChange={(e)=>{setEmail(e.target.value)}} />
                     <div className="sf-type">Date of birth</div>
-                    <input className="sf-input" type="date" />
+                    <input className="sf-input" type="date" onChange={(e)=>{setDob(e.target.value)}} />
                     <div className="sf-type">Password</div>
-                    <input className="sf-input" type="password" placeholder="Enter your password" />
+                    <input className="sf-input" type="password" placeholder="Enter your password" onChange={(e)=>{setPassword(e.target.value)}} />
                     <div className="sf-type">Re-Enter Password</div>
-                    <input className="sf-input" type="password" placeholder="Enter your password" />
-                     <button className="login-button" onClick={()=>{navigate('/home')}}>Sign In</button>
+                    <input className="sf-input" type="password" placeholder="Enter your password" onChange={(e)=>{setRePassword(e.target.value)}} />
+                    <div className="sf-type, red">{passwordMsg}</div>
+                     <button className="login-button" onClick={handleSignin}>Sign In</button>
                 </div>
                 
             </div>
